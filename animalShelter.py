200e8c5e9d63f86d2ab8c96d07df2562c7dd8a40
laypatel13 <laypatel1322008@gmail.com>
2026-04-21 21:49:20 +0530

    feat: create class structure skeleton

--- animalShelter.py
+++ animalShelter.py
@@ -1,3 +1,19 @@
 import json
 import os
 import random
+
+class Animal:
+    pass
+
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+
+class Parrot(Animal):
+    pass
+
+
+class Shelter:
+    pass
