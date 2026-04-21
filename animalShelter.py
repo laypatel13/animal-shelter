818057dccef47aed42523408cce8cd09ed4e78ac
laypatel13 <laypatel1322008@gmail.com>
2026-04-21 21:50:36 +0530

    add animal class

--- animalShelter.py
+++ animalShelter.py
@@ -3,7 +3,35 @@ import os
 import random
 
 class Animal:
-    pass
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.is_adopted = False
+        self.__id = random.randint(1000, 9999)
+
+    def display(self):
+        status = "Adopted" if self.is_adopted else "Not adopted"
+        print(f'ID: {self.__id} | Name: {self.name} | Age: {self.age} | Breed: {self.breed} | Status: {status}')
+
+    def mark_as_adopted(self):
+        self.is_adopted = True
+
+    def speak(self):
+        print('Some sound!')
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "age": self.age,
+            "breed": self.breed,
+            "is_adopted": self.is_adopted,
+            "id": self.__id,
+            "type": type(self).__name__
+        }
+
+    def get_id(self):
+        return self.__id
 
 class Dog(Animal):
     pass
